237238f75ff2d3cc81b909d19ccd8359db1d29a0
Roshan Upreti <roshan.upreti@invian.fi>
2023-05-13 18:39:11 +0300

    - Column data type updated in schema_client.sql

--- city-bike-rest-app/database/schema_client.sql
+++ city-bike-rest-app/database/schema_client.sql
@@ -5,30 +5,30 @@ CREATE TABLE `journey`
 (
     `departure`                     DATETIME NOT NULL COMMENT 'Date and time of departure',
     `return`                        DATETIME NOT NULL COMMENT 'Date and time of return',
-    `departure_station_id`          SMALLINT NOT NULL COMMENT 'Id of departure station',
-    `departure_station_name`        VARCHAR(255) NOT NULL COMMENT 'Name of departure station',
-    `return_station_id`             SMALLINT NOT NULL COMMENT 'Id of return station',
-    `return_station_name`           VARCHAR(255) NOT NULL COMMENT 'Name of return station',
-    `distance_covered_in_meters`    FLOAT NOT NULL COMMENT 'Distance covered in meters',
-    `duration_in_seconds`           FLOAT NOT NULL COMMENT 'Duration in seconds'
+    `departure_station_id`          INT(11) NOT NULL COMMENT 'Id of departure station',
+    `departure_station_name`        VARCHAR(50) NOT NULL COMMENT 'Name of departure station',
+    `return_station_id`             INT(11) NOT NULL COMMENT 'Id of return station',
+    `return_station_name`           VARCHAR(50) NOT NULL COMMENT 'Name of return station',
+    `distance_covered_in_meters`    INT(11) NOT NULL COMMENT 'Distance covered in meters',
+    `duration_in_seconds`           INT(11) NOT NULL COMMENT 'Duration in seconds'
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8
 ;
 
 CREATE TABLE `station`
 (
-    `fid`           INT NOT NULL COMMENT 'ID',
-    `id`            SMALLINT NOT NULL COMMENT 'Station ID',
-    `nimi`          VARCHAR(255) NOT NULL COMMENT 'Station name in Finnish',
-    `namn`          VARCHAR(255) NOT NULL COMMENT 'Station name in Swedish',
-    `name`          VARCHAR(255) NOT NULL COMMENT 'Station name',
-    `osoite`        VARCHAR(255) NOT NULL COMMENT 'Address in Swedish',
-    `adress`        VARCHAR(255) NOT NULL COMMENT 'Address in Swedish',
-    `kaupunki`      VARCHAR(255) NOT NULL COMMENT 'City name in Finnish',
-    `stad`          VARCHAR(255) NOT NULL COMMENT 'City name in Swedish',
-    `operaattor`    VARCHAR(255) NOT NULL COMMENT 'Operator',
-    `kapasiteet`    INT NOT NULL COMMENT 'Bicycle capacity of station',
-    `x`             FLOAT NOT NULL COMMENT 'X co-ordinate',
-    `y`             FLOAT NOT NULL COMMENT 'Y co-ordinate'
+    `fid`           INT(11) NOT NULL COMMENT 'ID',
+    `id`            INT(11) NOT NULL COMMENT 'Station ID',
+    `nimi`          VARCHAR(50) NOT NULL COMMENT 'Station name in Finnish',
+    `namn`          VARCHAR(50) NOT NULL COMMENT 'Station name in Swedish',
+    `name`          VARCHAR(50) NOT NULL COMMENT 'Station name',
+    `osoite`        VARCHAR(50) NOT NULL COMMENT 'Address in Swedish',
+    `adress`        VARCHAR(50) NOT NULL COMMENT 'Address in Swedish',
+    `kaupunki`      VARCHAR(50) NOT NULL COMMENT 'City name in Finnish',
+    `stad`          VARCHAR(50) NOT NULL COMMENT 'City name in Swedish',
+    `operaattor`    VARCHAR(50) NOT NULL COMMENT 'Operator',
+    `kapasiteet`    INT(11) NOT NULL COMMENT 'Bicycle capacity of station',
+    `x`             DOUBLE NOT NULL COMMENT 'X co-ordinate',
+    `y`             DOUBLE NOT NULL COMMENT 'Y co-ordinate'
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8
 ;
 
